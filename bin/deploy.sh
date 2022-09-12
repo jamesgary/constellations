615ba564fa28626d84866583ccc95d5a06ee013a
James Gary <gumotaku@gmail.com>
2022-09-12 03:46:10 -0700

    Fix deploy comment

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Make sure you've ran `npm run build` first!"
+echo "Make sure you've ran 'npm run build' first!"
 
 aws s3 cp build s3://constellationsgame.com/ --recursive --grants=read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 
